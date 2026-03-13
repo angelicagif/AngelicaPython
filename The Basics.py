febfec54577e7875ad2ffd545e5aef236055f36d
Angelica <stroudangelica@gmail.com>
2026-03-13 11:45:56 +0000

    adding the basics

---
+++ The Basics.py
@@ -0,0 +1,89 @@
+################## 1 #################
+
+# firstname = input("what is your first name? :  ")
+
+################## 2 #################
+
+# firstname = input("what is your first name? :  ")
+
+# surname = input("what is your surname? :  ")
+
+# print("Hello", firstname, surname);
+
+
+################## 3 #################
+
+# print("what do you call a bear with no teeth?\nA gummy bear!") 
+
+################## 4 #################
+
+# num1 = int(input("enter num1:  "))
+# num2 = int(input("enter num2:  "))
+
+# answer = num1 + num2
+
+# print("the total is", answer)
+
+################## 5 #################
+
+# num1 = int(input("enter num1:  "))
+# num2 = int(input("enter num2:  "))
+# num3 = int(input("enter num3:  "))
+
+# answer = (num1 + num2) * num3
+
+# print("the answere is", answer)
+
+################## 6 #################
+
+# start = int(input("How many slices of pizza did you start with?:  "))
+# eaten = int(input("How many slices of pizza did you eat?:  "))
+
+# left = start - eaten
+
+# print("You only have left", left, "slices of pizza.\nGreedy!" )
+
+################## 7 #################
+
+# name = input("enter your name:  ")
+# age = int(input("enter your age:  "))
+
+# print(name, "next birthday you will be", (age+1))
+
+################## 8 #################
+
+# bill = float(input("Total price of the bill: "))
+# diners = int(input("How many diners were there?:  "))
+
+# answer = bill / diners
+
+# print("Each diner must pay ", answer, "each")
+
+
+################## 9 #################
+
+# days = int(input("Number of days:  "))
+
+# hours = days * 24
+# minutes = days * 60
+# seconds = minutes * 60
+
+# print("In", days,"days there are:")
+# print(hours, "hours")
+# print(minutes, "minutes")
+# print(seconds, "seconds")
+
+################## 10 #################
+
+# kilo = int(input("enter a weight in kilograms:  "))
+# pounds = kilo * 2.204
+
+# print(kilo,"kilograms is", pounds, "pounds")
+
+################## 11 #################
+
+num1 = int(input("enter a number over 100:  "))
+num2 = int(input("enter a number under 10:  "))
+
+answer = num1 // num2
+print(num2, "goes into", num1, answer, "times")
