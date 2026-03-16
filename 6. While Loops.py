818e9c92a4035d720cc2c6235323c1b8ba8d88d3
Angelica <stroudangelica@gmail.com>
2026-03-16 19:42:05 +0000

    while loops on the train home.

---
+++ 6. While Loops.py
@@ -0,0 +1,53 @@
+################## 45 #################
+
+# total = 0
+
+# while total <= 50:
+#     num = int(input("enter a number:  "))
+#     total = total + num
+#     print("the total is ",total)
+
+################## 46 #################
+
+# num = 0
+# while num < 5:
+#     num = int(input("enter a number:  "))
+# print("the last number you entered was a", num)
+
+################## 47 #################
+
+# num1 = int(input("enter a number:  "))
+# total = num1
+# again = "y"
+
+# while again == "y":
+#     num2 = int(input("enter a second number:  "))
+#     total = total + num2
+#     again = input("do you want to add another number y/n:   ")
+# print("the total is", total)
+
+################## 48 #################
+
+# count = 0
+# again = "y"
+# while again == "y":
+#     name = input("enter a name:  ")
+#     print(name, "has now been invited")
+#     count = count + 1
+#     again = input("do you want to invite someone else? y/n:  ")
+# print("you have invited",count,"people to your party!")
+
+################## 49 #################
+
+# compnum = 50
+# guess = int(input("can you guess the number i am thinking of:  "))
+# count = 0
+
+# while guess != compnum:
+#     if guess < compnum:
+#         print("your guess is too low!")
+#     else:
+#         print("your guess is too high!")
+#     count = count + 1
+#     guess = int(input("have another guess:  "))
+# print("Well done, you took", count, "attempts")
