c68406f6ad80fb1a5c748d15deb26aa0a7ad57ef
Angelica <stroudangelica@gmail.com>
2026-03-16 07:45:36 +0000

    adding 5. For Loops

---
+++ 5. For Loops.py
@@ -0,0 +1,93 @@
+################## 35 #################
+
+# name = input("enter your name:  ")
+
+# for i in range (0, 3):
+#     print(name)
+
+################## 36 #################
+
+# name = input("enter your name:  ")
+# num = int(input("enter a number:  "))
+
+
+# for i in range (0, num):
+#     print(name)
+
+# ################## 37 #################
+
+# name = input("enter your name:  ")
+# for i in name:
+#     print(i)
+
+################## 38 #################
+
+# name = input("enter your name:  ")
+# num = int(input("enter a number:  "))
+
+# for x in range (0, num):
+#     for i in name:
+#         print(i)
+
+################## 39 #################
+
+# num = int(input("enter a number between 1 and 12:  "))
+# for i in range(0,13):
+#     print(num * i)
+
+################## 40 #################
+
+# num = int(input("enter a number below 50:  "))
+# for i in range(0,13):
+#     print(num * i)
+
+################## 41 #################
+
+# name = input("enter your name:  ")
+# num = int(input("enter a number:  "))
+
+# if num < 10:
+#     for x in range(0,num):
+#         print(name)
+# else:
+#     for i in range (0,3):
+#         print("too high")
+
+
+################## 42 #################
+
+# total = 0
+
+# for i in range (0,5):
+#     num = int(input("enter a number:  "))
+#     ans = input("do you want that included? ").lower()
+#     if ans == "yes":
+#         total = total + num
+#     else:
+#         None
+# print(total)
+
+# ################## 43 #################
+
+# dir = input("Which direction do you want the count? (u/d) ").lower()
+
+# if dir == "u":
+#     top = int(input("enter your top number:  "))
+#     for i in range(1, top+1):
+#         print(i)
+# elif dir == "d":
+#     num = int(input("enter a number below 20:  "))
+#     for x in range(20, num-1, -1):
+#         print(x)
+# else:
+#     print("I dont understand")
+
+################## 44 #################
+
+# num = int(input("how many people do you want to invite to the party?:  "))
+# if num < 10:
+#     for i in range(0, num):
+#         name = input("please enter their name:  ")
+#         print(name, "has been invited")
+# else:
+#     print("too many people")
